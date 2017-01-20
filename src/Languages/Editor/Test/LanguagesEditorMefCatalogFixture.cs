d2e4661150740f15d4c3fe3d1f9adff0f51d940d
Alexander Sher <alsher@microsoft.com>
2017-01-20 15:35:23 -0600

    Fix merge errors

--- src/Languages/Editor/Test/LanguagesEditorMefCatalogFixture.cs
+++ src/Languages/Editor/Test/LanguagesEditorMefCatalogFixture.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
@@ -18,6 +19,26 @@ namespace Microsoft.Languages.Editor.Test {
     // Fixture doesn't import itself. Use AssemblyFixtureImportAttribute
     [ExcludeFromCodeCoverage]
     public class LanguagesEditorMefCatalogFixture : AssemblyMefCatalogFixture {
+        protected override IEnumerable<string> GetAssemblies() {
+            return new[] {
+                "Microsoft.VisualStudio.CoreUtility.dll",
+                "Microsoft.VisualStudio.Text.Data.dll",
+                "Microsoft.VisualStudio.Text.Internal.dll",
+                "Microsoft.VisualStudio.Text.Logic.dll",
+                "Microsoft.VisualStudio.Text.UI.dll",
+                "Microsoft.VisualStudio.Text.UI.Wpf.dll",
+                "Microsoft.Languages.Editor.dll",
+                "Microsoft.R.Host.Client.dll",
+                "Microsoft.R.Common.Core.dll",
+                "Microsoft.R.Common.Core.Test.dll",
+                "Microsoft.R.Components.dll",
+                "Microsoft.R.Components.Test.dll",
+                "Microsoft.VisualStudio.Editor.dll",
+                "Microsoft.VisualStudio.Language.Intellisense.dll",
+                "Microsoft.VisualStudio.Platform.VSEditor.dll"
+            };
+        }
+
         public virtual IExportProvider Create(CoreServicesFixture coreServices) => new LanguagesEditorTestExportProvider(CreateContainer(), coreServices);
 
         protected class LanguagesEditorTestExportProvider : TestExportProvider {
--- src/R/Support/Test/RSupportMefCatalogFixture.cs
+++ src/R/Support/Test/RSupportMefCatalogFixture.cs
@@ -1,8 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
 using System.ComponentModel.Composition.Hosting;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Common.Core.Extensions;
 using Microsoft.Common.Core.Test.Fixtures;
@@ -17,6 +19,12 @@ namespace Microsoft.R.Support.Test {
     // Fixture doesn't import itself. Use AssemblyFixtureImportAttribute
     [ExcludeFromCodeCoverage]
     public class RSupportMefCatalogFixture : LanguagesEditorMefCatalogFixture {
+        protected override IEnumerable<string> GetAssemblies() => base.GetAssemblies().Concat(new[] {
+            "Microsoft.VisualStudio.InteractiveWindow.dll",
+            "Microsoft.R.Support",
+            "Microsoft.R.Support.Test"
+        });
+
         public override IExportProvider Create(CoreServicesFixture coreServices)
             => new RSupportTestExportProvider(CreateContainer(), coreServices);
 
